11180680ab2f67ea5a8fec72c5301e3c67934f0e
frank-64 <fcminipad@gmail.com>
2023-02-26 13:57:08 +0000

    Busyness meter added and started call to API.

--- gym-tracker-website/src/App.js
+++ gym-tracker-website/src/App.js
@@ -1,21 +1,90 @@
 import "./App.css";
+import ReactSpeedometer from "react-d3-speedometer";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [occupancyLevel, setOccupancy] = useState(0);
+  var headers = {};
+
+  useEffect(() => {
+    fetch(
+      "https://gym-tracker-functions.azurewebsites.net/api/determineGymOccupancy?",
+      {
+        method: "GET",
+        mode: "no-cors",
+        headers: headers,
+      }
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setOccupancy(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
   return (
     <div className="App">
+      <script crossorigin src="..."></script>
+      {console.log(occupancyLevel)}
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edited <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Gym Tracker Application</h1>
+        <p>This gym is currently:</p>
+        <div className="busyMeter">
+          <ReactSpeedometer
+            forceRender={true}
+            segments={5}
+            width={800}
+            height={700}
+            maxValue={100}
+            value={occupancyLevel}
+            segmentColors={[
+              "green",
+              "limegreen",
+              "gold",
+              "tomato",
+              "firebrick",
+            ]}
+            //currentValueText="Happiness Level"
+            customSegmentLabels={[
+              {
+                text: "Very Quiet",
+                position: "OUTSIDE",
+                color: "#ffffff",
+              },
+              {
+                text: "Quiet",
+                position: "OUTSIDE",
+                color: "#ffffff",
+              },
+              {
+                text: "Moderate",
+                position: "OUTSIDE",
+                color: "#ffffff",
+              },
+              {
+                text: "Busy",
+                position: "OUTSIDE",
+                color: "#ffffff",
+              },
+              {
+                text: "Very Busy",
+                position: "OUTSIDE",
+                color: "#ffffff",
+              },
+            ]}
+          />
+        </div>
       </header>
     </div>
   );
